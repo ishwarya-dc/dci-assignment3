6e89969be82a99169aa412b58f0e5aaea93a0839
ishwarya-dc <ishwaryamj96@gmail.com>
2025-12-01 20:50:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-    return 'Hello from Ishwarya's ECS Container!'
+    return "Hello from Ishwarya's ECS Container!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
